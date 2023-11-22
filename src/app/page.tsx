eb58fa1881829329f0baae16c52be5dee02748c0
hwatso02 <hwatso02@gmail.com>
2023-11-21 18:05:29 -0700

    Clean Up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,6 @@ export default function Home() {
           <h1 className="text-4xl font-bold mb-4">Welcome to My Missionary Mental Health App</h1>
           <p className="text-lg text-gray-600">Providing Support and Wellness for Missionaries</p>
       </div>
-
-          <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-              {/* div for bottom links */}
-      </div>
     </main>
   )
 }
