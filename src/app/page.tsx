1c55510955df74af34a9dd729174621d593e9339
hwatso02 <hwatso02@gmail.com>
2023-11-27 22:57:22 -0700

    basic navbar created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-center mb-8">
-          <h1 className="text-4xl font-bold mb-4">Welcome to My Missionary Mental Health App</h1>
-          <p className="text-lg text-gray-600">Providing Support and Wellness for Missionaries</p>
-      </div>
-    </main>
-  )
+    return (
+        <div>
+            {/* Navbar */}
+            <nav className="bg-gray-800 p-4">
+                <div className="container mx-auto flex items-center justify-between">
+                    <div className="text-white text-lg font-bold">
+                        Missionary App</div>
+                    <ul className="flex space-x-4">
+                        <li className="text-white">Home</li>
+                        <li className="text-white">Guided Breathing</li>
+                    </ul>
+                </div>
+            </nav>
+
+            {/* Main content */}
+            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                <div className="text-center mb-8">
+                    <h1 className="text-4xl font-bold mb-4">Welcome to My Missionary Mental Health App</h1>
+                    <p className="text-lg text-gray-600">Providing Support and Wellness for Missionaries</p>
+                </div>
+            </main>
+        </div>
+    );
 }
