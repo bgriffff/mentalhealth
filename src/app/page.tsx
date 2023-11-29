e58663795002a25821c27ac4292fbaa2f5948afc
hwatso02 <hwatso02@gmail.com>
2023-11-29 10:00:14 -0700

    Not-fully-working-navbar updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
-export default function Home() {
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+
+const Home: React.FC = () => {
     return (
         <div>
             {/* Navbar */}
             <nav className="bg-gray-800 p-4">
                 <div className="container mx-auto flex items-center justify-between">
                     <div className="text-white text-lg font-bold">
-                        Missionary App</div>
+                        Missionary App
+                    </div>
                     <ul className="flex space-x-4">
-                        <li className="text-white">Home</li>
-                        <li className="text-white">Guided Breathing</li>
+                        {/* Use Link for internal navigation */}
+                        <Link to="/" className="text-white">
+                            Home
+                        </Link>
+                        <Link to="/guided-breathing" className="text-white">
+                            Guided Breathing
+                        </Link>
                     </ul>
                 </div>
             </nav>
@@ -16,10 +25,16 @@ export default function Home() {
             {/* Main content */}
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
                 <div className="text-center mb-8">
-                    <h1 className="text-4xl font-bold mb-4">Welcome to My Missionary Mental Health App</h1>
-                    <p className="text-lg text-gray-600">Providing Support and Wellness for Missionaries</p>
+                    <h1 className="text-4xl font-bold mb-4">
+                        Welcome to My Missionary Mental Health App
+                    </h1>
+                    <p className="text-lg text-gray-600">
+                        Providing Support and Wellness for Missionaries
+                    </p>
                 </div>
             </main>
         </div>
     );
-}
+};
+
+export default Home;
